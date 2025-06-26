696d2e6a6fde5033a9ae881502df706f2092252e
wizyowizy <wizyowizy@yahoo.com>
2025-06-25 22:48:49 -0400

    set up eks

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,17 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
 provider "aws" {
   region = "us-east-2"
 }
